9174e86c175aa838e168cc0c605399c5dfa64e3e
32248480 <92283556+32248480@users.noreply.github.com>
2021-11-15 02:37:37 +0800

    Update Class03.java

--- Class03.java
+++ Class03.java
@@ -22,3 +22,10 @@ public static void main(String args[]){
 
 //value=10
 //value=12
+
+//(b),
+//21行的程式碼是呼叫第6行沒有引數的建構元Caaa()。22行的程式碼是呼叫第11行有引數的建構元Caaa(int i)。
+//(c),
+//Caaa類別無法宣告成public,因為一個檔案裡只可以有一個public的類別,並會和程式儲存的文字檔名相同。如果要將Caaa類別宣告成public,就要將Caaa類別存另外一個檔案,同時該檔案的名稱要為Caaa.java。
+//(d),
+//因為成員value並沒有被其他的類別使用,所以當value宣告成public,也不會影響到本範例的執行。
