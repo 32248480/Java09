d6556697385de149fec4ba95c17f58022bdcbee0
32248480 <92283556+32248480@users.noreply.github.com>
2021-11-15 02:47:42 +0800

    Update Class05.java

--- Class05.java
+++ Class05.java
@@ -1,3 +1,4 @@
+//(a),(b),(c),
 class CCount{
 private static int cnt=0;
 
@@ -39,3 +40,8 @@ public static void main(String args[]){
 //cnt=0
 //using setValue(10)...
 //cnt=10
+
+//(d),
+//cnt應要利用類別變數,因為cnt是用來計算CCount類別所建立的物件個數,所以可以宣告成類別變數,讓所有的CCount類別物件共用。
+//(e),
+//count()method可以宣告成實例函數，也可以宣告成類別函數。不過因為count()宣告成實例函數時,是由物件呼叫的,對count()來說並不具有太大的意義，所以還是會建議將count()宣告成類別函數。
